1fd9690041df8fb5047982b298d1f0ecbb73e870
Alfonso Marzano <alfonsomarzano@users.noreply.github.com>
2022-06-21 12:49:36 +0200

    Added latest dump
    Larger tables/views compressed to 7z

---
+++ CATEGORIE_202206211039.sql
@@ -0,0 +1,32 @@
+INSERT INTO rei.dbo.CATEGORIE (ID,CATEGORIA) VALUES
+	 ('1','Film TV e programmi'),
+	 ('2','Musica'),
+	 ('3','E-Books'),
+	 ('4','Film'),
+	 ('6','Linux'),
+	 ('7','Anime'),
+	 ('8','Cartoni'),
+	 ('9','Macintosh'),
+	 ('10','Windows Software'),
+	 ('11','Pc Game');
+INSERT INTO rei.dbo.CATEGORIE (ID,CATEGORIA) VALUES
+	 ('12','Playstation'),
+	 ('13','Students Releases'),
+	 ('14','Documentari'),
+	 ('21','Video Musicali'),
+	 ('22','Sport'),
+	 ('23','Teatro'),
+	 ('24','Wrestling'),
+	 ('25','Varie'),
+	 ('26','Xbox'),
+	 ('27','Immagini sfondi');
+INSERT INTO rei.dbo.CATEGORIE (ID,CATEGORIA) VALUES
+	 ('28','Altri Giochi'),
+	 ('29','Serie TV'),
+	 ('30','Fumetteria'),
+	 ('31','Trash'),
+	 ('32','Nintendo'),
+	 ('34','A-Book'),
+	 ('35','Podcast'),
+	 ('36','Edicola'),
+	 ('37','Mobile');
---
+++ CATEGORYCOUNT_202206211040.sql
@@ -0,0 +1,32 @@
+INSERT INTO rei.dbo.CATEGORYCOUNT (ID,CATEGORIA,CONTO) VALUES
+	 ('34','A-Book',591),
+	 ('28','Altri Giochi',30),
+	 ('7','Anime',6141),
+	 ('8','Cartoni',4866),
+	 ('14','Documentari',4726),
+	 ('3','E-Books',19915),
+	 ('36','Edicola',2685),
+	 ('4','Film',31472),
+	 ('1','Film TV e programmi',2149),
+	 ('30','Fumetteria',7703);
+INSERT INTO rei.dbo.CATEGORYCOUNT (ID,CATEGORIA,CONTO) VALUES
+	 ('27','Immagini sfondi',365),
+	 ('6','Linux',87),
+	 ('9','Macintosh',312),
+	 ('37','Mobile',253),
+	 ('2','Musica',12087),
+	 ('32','Nintendo',186),
+	 ('11','Pc Game',2805),
+	 ('12','Playstation',664),
+	 ('35','Podcast',740),
+	 ('29','Serie TV',28706);
+INSERT INTO rei.dbo.CATEGORYCOUNT (ID,CATEGORIA,CONTO) VALUES
+	 ('22','Sport',582),
+	 ('13','Students Releases',2644),
+	 ('23','Teatro',524),
+	 ('31','Trash',163),
+	 ('25','Varie',32),
+	 ('21','Video Musicali',1502),
+	 ('10','Windows Software',2393),
+	 ('24','Wrestling',204),
+	 ('26','Xbox',141);
